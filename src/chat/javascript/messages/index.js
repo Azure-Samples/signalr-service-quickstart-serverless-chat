773369233e268312a2727b704e89342abcf504c9
Anthony Chu <antchu@microsoft.com>
2019-03-03 23:46:52 -0800

    Update JavaScript quickstart

--- src/chat/javascript/messages/index.js
+++ src/chat/javascript/messages/index.js
@@ -1,10 +1,9 @@
 // Copyright (c) Microsoft. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
-module.exports = function (context, req) {
-  context.bindings.signalRMessages = [{
+module.exports = async function (context, req) {
+  return {
     "target": "newMessage",
     "arguments": [ req.body ]
-  }];
-  context.done();
+  };
 };
--- src/chat/javascript/negotiate/index.js
+++ src/chat/javascript/negotiate/index.js
@@ -1,7 +1,6 @@
 // Copyright (c) Microsoft. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
-module.exports = function (context, req, connectionInfo) {
-  context.res = { body: connectionInfo };
-  context.done();
+module.exports = async function (context, req, connectionInfo) {
+  context.res.body = connectionInfo;
 };
