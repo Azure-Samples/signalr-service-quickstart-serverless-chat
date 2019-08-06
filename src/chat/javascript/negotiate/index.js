40bb52c8e97efb6d5ea58dace7f6437bdaf456a3
Anthony Chu <antchu@microsoft.com>
2019-08-05 22:19:35 -0700

    Return JSON from JS sample

--- src/chat/javascript/negotiate/index.js
+++ src/chat/javascript/negotiate/index.js
@@ -2,5 +2,5 @@
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
 module.exports = async function (context, req, connectionInfo) {
-  context.res.body = connectionInfo;
+  context.res.json(connectionInfo);
 };
